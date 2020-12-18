5f92caa6edc3affdeb6104c90870da5a67564d55
Marc <marc.ferraro1@gmail.com>
2020-12-17 22:16:23 -0600

    typo fix?

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,28 +2,22 @@ require 'pry'
 
 
 class Backer
+
     def initialize(name)
         @name = name
+        @@all << self
+        # binding.pry
     end
+
+    @@all = []
+
+    attr_reader :name
+
+    def self.all
+        @@all
+    end
+
 end
 
-# class Backer
-
-#     def initialize(name)
-#         @name = name
-#         @@all << self
-#         # binding.pry
-#     end
-
-#     @@all = []
-
-#     attr_reader :name
-
-#     def self.all
-#         @@all
-#     end
-
-# end
-
-# marc = Backer.new("Marc")
-# # binding.pry
+marc = Backer.new("Marc")
+# binding.pry
